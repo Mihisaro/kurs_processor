92cad36e1d7371a7116a76c29667e0eac012222f
Mihisaro <168758017+Mihisaro@users.noreply.github.com>
2026-03-21 20:42:52 +0700

    add

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,54 +1,18 @@
-# lexical_analyzer.py
+# lexical_analyzer.py - простой сканер для проверки недопустимых символов
 from enum import Enum
-import re
 
 class TokenType(Enum):
     CONST = (1, "Ключевое слово const")
-    
-    I8 = (2, "Тип i8")
-    I16 = (3, "Тип i16")
-    I32 = (4, "Тип i32")
-    I64 = (5, "Тип i64")
-    I128 = (6, "Тип i128")
-    ISIZE = (7, "Тип isize")
-    U8 = (8, "Тип u8")
-    U16 = (9, "Тип u16")
-    U32 = (10, "Тип u32")
-    U64 = (11, "Тип u64")
-    U128 = (12, "Тип u128")
-    USIZE = (13, "Тип usize")
-    
-    BOOL = (30, "ОШИБКА: Тип bool (недопустим, требуется целочисленный тип)")
-    CHAR = (31, "ОШИБКА: Тип char (недопустим, требуется целочисленный тип)")
-    F32 = (32, "ОШИБКА: Тип f32 (недопустим, требуется целочисленный тип)")
-    F64 = (33, "ОШИБКА: Тип f64 (недопустим, требуется целочисленный тип)")
-    STR = (34, "ОШИБКА: Тип &str (недопустим, требуется целочисленный тип)")
-    STRING = (35, "ОШИБКА: Тип String (недопустим, требуется целочисленный тип)")
-    
-    IDENTIFIER = (14, "Идентификатор (имя константы)")
-    
-    ASSIGN = (15, "Оператор присваивания =")
-    COLON = (16, "Разделитель :")
-    SEMICOLON = (17, "Разделитель ;")
-    
-    DECIMAL_LITERAL = (18, "Десятичный литерал")
-    HEX_LITERAL = (19, "Шестнадцатеричный литерал")
-    BINARY_LITERAL = (20, "Двоичный литерал")
-    OCTAL_LITERAL = (21, "Восьмеричный литерал")
-    BYTE_LITERAL = (22, "Байтовый литерал")
-    
-    BOOL_LITERAL = (40, "ОШИБКА: Булев литерал (недопустим, требуется целочисленное значение)")
-    FLOAT_LITERAL = (41, "ОШИБКА: Вещественный литерал (недопустим, требуется целочисленное значение)")
-    CHAR_LITERAL = (42, "ОШИБКА: Символьный литерал (недопустим, требуется целочисленное значение)")
-    STRING_LITERAL = (43, "ОШИБКА: Строковый литерал (недопустим, требуется целочисленное значение)")
-    
-    SPACE = (90, "Пробел")
-    TAB = (91, "Табуляция")
-    NEWLINE = (92, "Перевод строки")
-    
-    COMMENT = (93, "ОШИБКА: Комментарий (не допускается в объявлении константы)")
-    
-    ERROR = (99, "Ошибка: недопустимый символ")
+    IDENTIFIER = (2, "Идентификатор")
+    COLON = (3, "Двоеточие")
+    TYPE = (4, "Тип данных")
+    ASSIGN = (5, "Оператор присваивания")
+    NUMBER = (6, "Числовой литерал")
+    SEMICOLON = (7, "Точка с запятой")
+    SPACE = (8, "Пробел")
+    TAB = (9, "Табуляция")
+    NEWLINE = (10, "Перевод строки")
+    ERROR = (99, "Недопустимый символ")
     
     def __init__(self, code, description):
         self.code = code
@@ -56,7 +20,7 @@ class TokenType(Enum):
     
     @property
     def is_error(self):
-        return (30 <= self.code <= 49) or self.code == 93 or self.code == 99
+        return self.code == 99
 
 
 class Token:
@@ -68,7 +32,7 @@ class Token:
         self.end_pos = end_pos
     
     def __str__(self):
-        return f"{self.type.name}: '{self.value}' at line {self.line}, pos {self.start_pos}-{self.end_pos}"
+        return f"{self.type.name}: '{self.value}' (строка {self.line}, позиция {self.start_pos}-{self.end_pos})"
     
     @property
     def is_error(self):
@@ -77,43 +41,32 @@ class Token:
 
 class LexicalAnalyzer:
     def __init__(self):
-        self.integer_types = {
-            'i8': TokenType.I8,
-            'i16': TokenType.I16,
-            'i32': TokenType.I32,
-            'i64': TokenType.I64,
-            'i128': TokenType.I128,
-            'isize': TokenType.ISIZE,
-            'u8': TokenType.U8,
-            'u16': TokenType.U16,
-            'u32': TokenType.U32,
-            'u64': TokenType.U64,
-            'u128': TokenType.U128,
-            'usize': TokenType.USIZE,
-        }
+        # Допустимые символы для корректного объявления константы
+        self.allowed_chars = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_:=; ')
         
-        self.invalid_types = {
-            'bool': TokenType.BOOL,
-            'char': TokenType.CHAR,
-            'f32': TokenType.F32,
-            'f64': TokenType.F64,
-            '&str': TokenType.STR,
-            'String': TokenType.STRING,
-            'str': TokenType.STR,
-        }
-        
+        # Ключевые слова
         self.keywords = {
             'const': TokenType.CONST,
         }
         
-        self.bool_literals = {
-            'true': TokenType.BOOL_LITERAL,
-            'false': TokenType.BOOL_LITERAL,
+        # Типы данных
+        self.types = {
+            'i32': TokenType.TYPE,
+            'i8': TokenType.TYPE,
+            'i16': TokenType.TYPE,
+            'i64': TokenType.TYPE,
+            'i128': TokenType.TYPE,
+            'u8': TokenType.TYPE,
+            'u16': TokenType.TYPE,
+            'u32': TokenType.TYPE,
+            'u64': TokenType.TYPE,
+            'u128': TokenType.TYPE,
         }
         
+        # Допустимые одиночные символы
         self.single_chars = {
-            '=': TokenType.ASSIGN,
             ':': TokenType.COLON,
+            '=': TokenType.ASSIGN,
             ';': TokenType.SEMICOLON,
         }
     
@@ -125,6 +78,7 @@ class LexicalAnalyzer:
             line_tokens = self._analyze_line(line, line_num)
             tokens.extend(line_tokens)
             
+            # Добавляем перевод строки
             if line_num < len(lines):
                 tokens.append(Token(
                     TokenType.NEWLINE,
@@ -142,21 +96,13 @@ class LexicalAnalyzer:
         length = len(line)
         
         while i < length:
-            if line[i].isspace():
+            # Пробелы и табуляция
+            if line[i] == ' ':
                 start = i
-                space_char = line[i]
-                if space_char == ' ':
-                    token_type = TokenType.SPACE
-                elif space_char == '\t':
-                    token_type = TokenType.TAB
-                else:
-                    token_type = TokenType.SPACE
-                
-                while i < length and line[i].isspace():
+                while i < length and line[i] == ' ':
                     i += 1
-                
                 tokens.append(Token(
-                    token_type,
+                    TokenType.SPACE,
                     line[start:i],
                     line_num,
                     start + 1,
@@ -164,21 +110,9 @@ class LexicalAnalyzer:
                 ))
                 continue
             
-            if i < length - 1 and line[i] == '/' and line[i+1] == '/':
-                comment = line[i:]
+            if line[i] == '\t':
                 tokens.append(Token(
-                    TokenType.COMMENT,
-                    comment,
-                    line_num,
-                    i + 1,
-                    length
-                ))
-                break  
-            
-            if line[i] in self.single_chars:
-                token_type = self.single_chars[line[i]]
-                tokens.append(Token(
-                    token_type,
+                    TokenType.TAB,
                     line[i],
                     line_num,
                     i + 1,
@@ -187,211 +121,19 @@ class LexicalAnalyzer:
                 i += 1
                 continue
             
-            if i < length - 2 and line[i] == '0' and line[i+1] == 'x':
-                start = i
-                i += 2
-                while i < length and (line[i].isdigit() or line[i].lower() in 'abcdef'):
-                    i += 1
-                hex_literal = line[start:i]
+            # Допустимые одиночные символы
+            if line[i] in self.single_chars:
                 tokens.append(Token(
-                    TokenType.HEX_LITERAL,
-                    hex_literal,
+                    self.single_chars[line[i]],
+                    line[i],
                     line_num,
-                    start + 1,
-                    i
+                    i + 1,
+                    i + 1
                 ))
-                continue
-            
-            if i < length - 2 and line[i] == '0' and line[i+1] == 'b':
-                start = i
-                i += 2
-                while i < length and line[i] in '01':
-                    i += 1
-                bin_literal = line[start:i]
-                tokens.append(Token(
-                    TokenType.BINARY_LITERAL,
-                    bin_literal,
-                    line_num,
-                    start + 1,
-                    i
-                ))
-                continue
-            
-            if i < length - 2 and line[i] == '0' and line[i+1] == 'o':
-                start = i
-                i += 2
-                while i < length and line[i] in '01234567':
-                    i += 1
-                oct_literal = line[start:i]
-                tokens.append(Token(
-                    TokenType.OCTAL_LITERAL,
-                    oct_literal,
-                    line_num,
-                    start + 1,
-                    i
-                ))
-                continue
-            
-            if i < length - 1 and line[i] == 'b' and (line[i+1] == "'" or line[i+1] == '"'):
-                start = i
-                quote = line[i+1]
-                i += 2
-                while i < length and line[i] != quote:
-                    if line[i] == '\\' and i + 1 < length:
-                        i += 2
-                    else:
-                        i += 1
-                if i < length and line[i] == quote:
-                    i += 1
-                    byte_literal = line[start:i]
-                    tokens.append(Token(
-                        TokenType.BYTE_LITERAL,
-                        byte_literal,
-                        line_num,
-                        start + 1,
-                        i
-                    ))
-                else:
-                    tokens.append(Token(
-                        TokenType.ERROR,
-                        line[start:],
-                        line_num,
-                        start + 1,
-                        length
-                    ))
-                    i = length
-                continue
-            
-            if line[i] == '"':
-                start = i
                 i += 1
-                while i < length and line[i] != '"':
-                    if line[i] == '\\' and i + 1 < length:
-                        i += 2
-                    else:
-                        i += 1
-                if i < length and line[i] == '"':
-                    i += 1
-                    string_literal = line[start:i]
-                    tokens.append(Token(
-                        TokenType.STRING_LITERAL,
-                        string_literal,
-                        line_num,
-                        start + 1,
-                        i
-                    ))
-                else:
-                    tokens.append(Token(
-                        TokenType.ERROR,
-                        line[start:],
-                        line_num,
-                        start + 1,
-                        length
-                    ))
-                    i = length
                 continue
             
-            if line[i] == "'":
-                start = i
-                i += 1
-                if i < length and line[i] != "'":
-                    if line[i] == '\\' and i + 1 < length:
-                        i += 2
-                    else:
-                        i += 1
-                if i < length and line[i] == "'":
-                    i += 1
-                    char_literal = line[start:i]
-                    tokens.append(Token(
-                        TokenType.CHAR_LITERAL,
-                        char_literal,
-                        line_num,
-                        start + 1,
-                        i
-                    ))
-                else:
-                    tokens.append(Token(
-                        TokenType.ERROR,
-                        line[start:],
-                        line_num,
-                        start + 1,
-                        length
-                    ))
-                    i = length
-                continue
-            
-            if i < length - 1 and line[i].isdigit() and line[i+1] == '.':
-                start = i
-                i += 2
-                while i < length and line[i].isdigit():
-                    i += 1
-                if i < length and line[i].lower() == 'e':
-                    i += 1
-                    if i < length and (line[i] == '+' or line[i] == '-'):
-                        i += 1
-                    while i < length and line[i].isdigit():
-                        i += 1
-                float_literal = line[start:i]
-                tokens.append(Token(
-                    TokenType.FLOAT_LITERAL,
-                    float_literal,
-                    line_num,
-                    start + 1,
-                    i
-                ))
-                continue
-            
-            if line[i].isdigit() or (line[i] == '-' and i + 1 < length and line[i+1].isdigit()):
-                start = i
-                
-                if line[i] == '-':
-                    i += 1
-                
-                while i < length and line[i].isdigit():
-                    i += 1
-                
-                suffix_start = i
-                while i < length and line[i].isalpha():
-                    i += 1
-                
-                number = line[start:i]
-                suffix = line[suffix_start:i] if suffix_start < i else ""
-                
-                if suffix:
-                    if suffix in self.integer_types:
-                        tokens.append(Token(
-                            TokenType.DECIMAL_LITERAL,
-                            number,
-                            line_num,
-                            start + 1,
-                            i
-                        ))
-                    elif suffix in self.invalid_types:
-                        tokens.append(Token(
-                            self.invalid_types[suffix],
-                            number,
-                            line_num,
-                            start + 1,
-                            i
-                        ))
-                    else:
-                        tokens.append(Token(
-                            TokenType.ERROR,
-                            number,
-                            line_num,
-                            start + 1,
-                            i
-                        ))
-                else:
-                    tokens.append(Token(
-                        TokenType.DECIMAL_LITERAL,
-                        number,
-                        line_num,
-                        start + 1,
-                        i
-                    ))
-                continue
-            
+            # Идентификаторы, ключевые слова и типы
             if line[i].isalpha() or line[i] == '_':
                 start = i
                 while i < length and (line[i].isalnum() or line[i] == '_'):
@@ -399,15 +141,7 @@ class LexicalAnalyzer:
                 
                 word = line[start:i]
                 
-                if word in self.bool_literals:
-                    tokens.append(Token(
-                        self.bool_literals[word],
-                        word,
-                        line_num,
-                        start + 1,
-                        i
-                    ))
-                elif word in self.keywords:
+                if word in self.keywords:
                     tokens.append(Token(
                         self.keywords[word],
                         word,
@@ -415,17 +149,9 @@ class LexicalAnalyzer:
                         start + 1,
                         i
                     ))
-                elif word in self.integer_types:
+                elif word in self.types:
                     tokens.append(Token(
-                        self.integer_types[word],
-                        word,
-                        line_num,
-                        start + 1,
-                        i
-                    ))
-                elif word in self.invalid_types:
-                    tokens.append(Token(
-                        self.invalid_types[word],
+                        self.types[word],
                         word,
                         line_num,
                         start + 1,
@@ -441,6 +167,22 @@ class LexicalAnalyzer:
                     ))
                 continue
             
+            # Числа
+            if line[i].isdigit():
+                start = i
+                while i < length and line[i].isdigit():
+                    i += 1
+                
+                tokens.append(Token(
+                    TokenType.NUMBER,
+                    line[start:i],
+                    line_num,
+                    start + 1,
+                    i
+                ))
+                continue
+            
+            # Недопустимый символ
             tokens.append(Token(
                 TokenType.ERROR,
                 line[i],
@@ -453,42 +195,8 @@ class LexicalAnalyzer:
         return tokens
     
     def validate_const_declaration(self, tokens):
-        significant_tokens = [t for t in tokens 
-                              if t.type not in [TokenType.SPACE, TokenType.TAB, TokenType.NEWLINE]]
-        
-        if len(significant_tokens) < 5:
-            return False, "Недостаточно токенов для объявления константы"
-        
-        if len(significant_tokens) != 7:
-            return False, f"Неверное количество элементов. Ожидается 7, получено {len(significant_tokens)}"
-        
-        if significant_tokens[0].type != TokenType.CONST:
-            return False, f"Ошибка: ожидается 'const', получен {significant_tokens[0].type.description}"
-        
-        if significant_tokens[1].type != TokenType.IDENTIFIER:
-            return False, f"Ошибка: ожидается идентификатор (имя константы), получен {significant_tokens[1].type.description}"
-        
-        if significant_tokens[2].type != TokenType.COLON:
-            return False, f"Ошибка: ожидается ':', получен {significant_tokens[2].type.description}"
-        
-        type_token = significant_tokens[3]
-        if type_token.type.code < 2 or type_token.type.code > 13:
-            if type_token.type.code >= 30:
-                return False, f"Ошибка: недопустимый тип '{type_token.value}'. Требуется целочисленный тип (i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize)"
-            else:
-                return False, f"Ошибка: ожидается целочисленный тип, получен {type_token.type.description}"
-        
-        if significant_tokens[4].type != TokenType.ASSIGN:
-            return False, f"Ошибка: ожидается '=', получен {significant_tokens[4].type.description}"
-        
-        value_token = significant_tokens[5]
-        if value_token.type.code < 18 or value_token.type.code > 22:
-            if value_token.type.code >= 40:
-                return False, f"Ошибка: недопустимое значение '{value_token.value}'. Требуется целочисленный литерал"
-            else:
-                return False, f"Ошибка: ожидается целочисленный литерал, получен {value_token.type.description}"
-        
-        if significant_tokens[6].type != TokenType.SEMICOLON:
-            return False, f"Ошибка:ожидается ';', получен {significant_tokens[6].type.description}"
-        
-        return True, "Корректное объявление целочисленной константы"
+        """Просто проверяем наличие недопустимых символов"""
+        for token in tokens:
+            if token.is_error:
+                return False, f"Строка {token.line}: недопустимый символ '{token.value}' на позиции {token.start_pos}"
+        return True, "OK"
---
+++ parser.py
@@ -0,0 +1,252 @@
+from lexical_analyzer import TokenType
+
+
+class ParserError:
+    def __init__(self, fragment, line, position, description):
+        self.fragment = fragment
+        self.line = line
+        self.position = position
+        self.description = description
+
+    def __str__(self):
+        return f"[строка {self.line}, позиция {self.position}] {self.description}: '{self.fragment}'"
+
+
+class SyntaxTreeNode:
+    def __init__(self, node_type, value=None, line=None, position=None):
+        self.node_type = node_type
+        self.value = value
+        self.line = line
+        self.position = position
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def __str__(self, level=0):
+        indent = "  " * level
+        result = f"{indent}{self.node_type}"
+        if self.value:
+            result += f": {self.value}"
+        if self.line:
+            result += f" (строка {self.line})"
+        result += "\n"
+        for child in self.children:
+            result += child.__str__(level + 1)
+        return result
+
+
+class Parser:
+    def __init__(self):
+        self.tokens = []
+        self.significant_tokens = []
+        self.position = 0
+        self.current_token = None
+        self.errors = []
+        self.syntax_tree = None
+
+    # =========================================
+    # ОСНОВНОЙ МЕТОД
+    # =========================================
+    def parse(self, tokens):
+        self.tokens = tokens
+        self.errors = []
+        self.position = 0
+
+        # 1. Лексические ошибки
+        for token in tokens:
+            if token.is_error:
+                self.errors.append(ParserError(
+                    fragment=token.value,
+                    line=token.line,
+                    position=token.start_pos,
+                    description=f"Лексическая ошибка: недопустимый символ '{token.value}'"
+                ))
+
+        # 2. Фильтрация
+        self.significant_tokens = [
+            t for t in tokens
+            if t.type not in [TokenType.SPACE, TokenType.TAB, TokenType.NEWLINE]
+            and not t.is_error
+        ]
+
+        if not self.significant_tokens:
+            return None, self.errors
+
+        self.update_current_token()
+
+        # 3. Парсинг
+        self.syntax_tree = self.parse_const_declaration()
+
+        return self.syntax_tree, self.errors
+
+    # =========================================
+    # ГРАММАТИКА
+    # =========================================
+    def parse_const_declaration(self):
+        root = SyntaxTreeNode("const_declaration")
+
+        # 🔥 FIX: если нет const → синхронизация
+        if not self.match_token(TokenType.CONST):
+            self.missing_token_error("const")
+            self.synchronize()
+            return root
+        else:
+            token = self.previous_token()
+            root.add_child(SyntaxTreeNode("keyword", token.value, token.line, token.start_pos))
+
+        # identifier
+        ident = self.match_token(TokenType.IDENTIFIER)
+        if ident:
+            root.add_child(SyntaxTreeNode("identifier", ident.value, ident.line, ident.start_pos))
+        else:
+            self.error_here("Ожидается идентификатор")
+
+        # :
+        self.expect(TokenType.COLON, ":")
+
+        # type
+        type_token = self.match_token(TokenType.TYPE)
+        if type_token:
+            root.add_child(SyntaxTreeNode("type", type_token.value,
+                                         type_token.line, type_token.start_pos))
+        else:
+            self.error_here("Ожидается тип данных")
+
+        # =
+        self.expect(TokenType.ASSIGN, "=")
+
+        # number
+        number = self.match_token(TokenType.NUMBER)
+        if number:
+            root.add_child(SyntaxTreeNode("value", number.value,
+                                         number.line, number.start_pos))
+        else:
+            self.error_here("Ожидается число")
+
+        # ;
+        self.expect(TokenType.SEMICOLON, ";")
+
+        return root
+
+    # =========================================
+    # EXPECT
+    # =========================================
+    def expect(self, token_type, symbol):
+        if self.match_with_repetition_check(token_type, symbol):
+            return True
+
+        self.missing_token_error(symbol)
+        return False
+
+    # =========================================
+    # ПОВТОРЫ === ::: ;;;
+    # =========================================
+    def match_with_repetition_check(self, token_type, symbol):
+        if not self.current_token:
+            return False
+
+        if self.current_token.type != token_type:
+            return False
+
+        start_token = self.current_token
+        count = 0
+
+        while self.current_token and self.current_token.type == token_type:
+            count += 1
+            self.advance()
+
+        if count > 1:
+            self.errors.append(ParserError(
+                fragment=symbol * count,
+                line=start_token.line,
+                position=start_token.start_pos,
+                description=f"Повторяющийся оператор '{symbol}' ({count} раз) недопустим"
+            ))
+
+        return True
+
+    # =========================================
+    # 🔥 АНТИ-КАСКАД (НОВОЕ)
+    # =========================================
+    def synchronize(self):
+        """Перепрыгиваем к ; или следующему const"""
+        while self.current_token:
+            if self.current_token.type == TokenType.SEMICOLON:
+                self.advance()
+                break
+
+            if self.current_token.type == TokenType.CONST:
+                break
+
+            self.advance()
+
+    # =========================================
+    # ОШИБКИ
+    # =========================================
+    def missing_token_error(self, expected):
+        if self.current_token:
+            self.errors.append(ParserError(
+                fragment=self.current_token.value,
+                line=self.current_token.line,
+                position=self.current_token.start_pos,
+                description=f"Пропущен '{expected}' перед '{self.current_token.value}'"
+            ))
+        else:
+            self.add_error_at_end(expected)
+
+    def error_here(self, message):
+        if self.current_token:
+            self.add_error(self.current_token, message)
+            self.advance()
+        else:
+            self.add_error_at_end(message)
+
+    def add_error(self, token, description):
+        self.errors.append(ParserError(
+            fragment=token.value,
+            line=token.line,
+            position=token.start_pos,
+            description=description
+        ))
+
+    def add_error_at_end(self, expected):
+        line = 1
+        pos = 1
+
+        if self.significant_tokens:
+            last = self.significant_tokens[-1]
+            line = last.line
+            pos = last.end_pos + 1
+
+        self.errors.append(ParserError(
+            fragment="EOF",
+            line=line,
+            position=pos,
+            description=f"Ожидается {expected}, но достигнут конец файла"
+        ))
+
+    # =========================================
+    # УТИЛИТЫ
+    # =========================================
+    def match_token(self, expected_type):
+        if self.current_token and self.current_token.type == expected_type:
+            token = self.current_token
+            self.advance()
+            return token
+        return None
+
+    def previous_token(self):
+        if self.position > 0:
+            return self.significant_tokens[self.position - 1]
+        return None
+
+    def advance(self):
+        self.position += 1
+        self.update_current_token()
+
+    def update_current_token(self):
+        if self.position < len(self.significant_tokens):
+            self.current_token = self.significant_tokens[self.position]
+        else:
+            self.current_token = None
